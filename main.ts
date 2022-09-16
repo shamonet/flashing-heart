91a5731605cac0725d3ce3d00c8f91c5a0d01c20
shamonet <53173037+shamonet@users.noreply.github.com>
2022-09-16 16:05:37 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    music.ringTone(262)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(300)
+})
